11d24ecb600b6889c462d177c25e08cb7ce8b06d
Kamil Jaskiewicz <ognius123@gmail.com>
2024-01-07 14:37:44 +0100

    H2 DB tables init for demonstration purposes.

--- src/main/java/com/jaskiewiczk/campaignCRUD/CampaignCrudApplication.java
+++ src/main/java/com/jaskiewiczk/campaignCRUD/CampaignCrudApplication.java
@@ -1,5 +1,7 @@
 package com.jaskiewiczk.campaignCRUD;
 
+import com.jaskiewiczk.campaignCRUD.service.CampaignKeywordsService;
+import com.jaskiewiczk.campaignCRUD.service.CampaignTownService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,6 +11,15 @@ public class CampaignCrudApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(CampaignCrudApplication.class, args);
+
+        // Initialize tables in the H2 in-memory database for testing purposes.
+        // This step includes populating the 'product_campaign_keywords' table.
+        CampaignKeywordsService initializationKeywordsService = context.getBean(CampaignKeywordsService.class);
+        initializationKeywordsService.sampleKeywordsInit();
+
+        // This step includes populating the 'campaign_towns' table
+        CampaignTownService initializationTownsService = context.getBean(CampaignTownService.class);
+        initializationTownsService.sampleTownsInit();
     }
 
 }
